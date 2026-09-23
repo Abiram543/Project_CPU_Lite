57e5096213c0fc8180e7012481119285cf2967cf
Abiram543 <abirams542003@gmail.com>
2026-09-23 17:00:08 +0530

    Added Control Unit

--- src/Instruction_Mem.v
+++ src/Instruction_Mem.v
@@ -2,6 +2,7 @@ module Instruction_Mem (
     input wire clk, 
     input wire rstn,
     input wire PC_val,      // From PC [Program Counter]
+    input wire I_Rd_en,     // From Control Unit
 
     output reg [`WIDTH-1:0] Instr
 );
@@ -12,7 +13,14 @@ initial begin
      $readmemh("programmem.h", P_Mem);
 end
 
-assign Instr = P_Mem[PC_val];
+always @(posedge clk or negedge rstn) begin
+    if (!rstn) begin
+        Instr <= 'hFF000000;    // Halt
+    end
+    else if (I_Rd_en) begin
+        Instr <= P_Mem[PC_val];
+    end
+end
 
 
 endmodule
--- src/RegFiles.v
+++ src/RegFiles.v
@@ -4,7 +4,7 @@ module RegFiles (
     input wire [`LOG2_NofR-1:0] Rs1_Addr, Rs2_Addr,
     input wire [`LOG2_NofR-1:0] Rd_Addr,        // From Write back Stage
     input wire [`WIDTH-1:0] Rd_val,             // From Write back stage
-    input wire wr_en,                           // From Control unit
+    input wire wr_en, rd_en                     // From Control unit
     output reg [`WIDTH-1:0] Rs1_val, Rs2_val    // To ALU op1, op2
 );
 
@@ -19,9 +19,11 @@ always @(posedge clk or negedge rstn) begin
         end
     end 
     else begin
-        Rs1 <= Reg[Rs1_Addr];
-        Rs2 <= Reg[Rs2_Addr];
-        if (wr_en) begin
+        if (rd_en) begin
+            Rs1 <= Reg[Rs1_Addr];
+            Rs2 <= Reg[Rs2_Addr];
+        end
+        else if (wr_en) begin
             Reg[Rd_Addr] <= Rd_val;
         end
     end
